eb40a27c95374da14b94d5b26102027fe267f8ab
ha4587323@gmail.com <ali.huzaifa@stackone.com>
2026-05-18 17:09:39 +0500

    while do while loop with events has covered in class

---
+++ Javascript/15) While Do While Loop & Events/app.js
@@ -0,0 +1,85 @@
+// for (var i = 0; i < 5; i++) {
+//     console.log("Good Morning");
+// }
+
+
+// var count = 0;
+// var loopRun = true;
+
+// while (loopRun === true) {
+//     console.log('Loop is running');
+//     if (count === 10) {
+//         loopRun = false;
+//     }
+//     count++
+// }
+
+
+
+// var count = 10;
+
+// while (count > 0) {
+//     console.log(count);
+//     count--
+// }
+
+
+// var count = 1;
+
+// while (count < 20) {
+//     console.log(count);
+//     // count = count + 2
+//     count += 2
+// }
+
+
+
+// var count = 10;
+
+// do {
+//     console.log("Loop is Running");
+//     count++
+// } while (count < 20);
+
+
+
+// var day = 5;
+// switch (day) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     default:
+//         console.log('Invalid Day');
+//         break;
+// }
+
+
+// function call() {
+//     alert("Hello World")
+// }
+
+
+// function onMouseOver() {
+//     console.log('Mouse Over');
+// }
+// function onMouseOut() {
+//     console.log('Mouse Out');
+// }
+
+// function focusCall() {
+//     console.log('Focus');
+// }
+
+
+// var inputEl = document.getElementById('userName')
+// inputEl.value = "Ali Huzaifa"
+// console.log(inputEl);
+
+
+var imageEl = document.getElementById('imageEl')
+function swapImage() {
+    imageEl.src = "images/car2.jpg"
+}
